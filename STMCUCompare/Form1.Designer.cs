5e2b765f52c451f06bb7912390a3a9bcc4acc8da
tehdmitry <tehdmitry@gmail.com>
2021-01-27 03:13:59 +0300

    first commit

---
+++ STMCUCompare/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace STMCUCompare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_MCU1 = new System.Windows.Forms.ComboBox();
+            this.cb_MCU2 = new System.Windows.Forms.ComboBox();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.dgvCompare = new System.Windows.Forms.DataGridView();
+            this.attribute_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MCU1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MCU2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCompare)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cb_MCU1
+            // 
+            this.cb_MCU1.FormattingEnabled = true;
+            this.cb_MCU1.Location = new System.Drawing.Point(6, 19);
+            this.cb_MCU1.Name = "cb_MCU1";
+            this.cb_MCU1.Size = new System.Drawing.Size(254, 21);
+            this.cb_MCU1.TabIndex = 0;
+            // 
+            // cb_MCU2
+            // 
+            this.cb_MCU2.FormattingEnabled = true;
+            this.cb_MCU2.Location = new System.Drawing.Point(266, 19);
+            this.cb_MCU2.Name = "cb_MCU2";
+            this.cb_MCU2.Size = new System.Drawing.Size(254, 21);
+            this.cb_MCU2.TabIndex = 1;
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(526, 19);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(75, 23);
+            this.btnCompare.TabIndex = 2;
+            this.btnCompare.Text = "Compare";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // dgvCompare
+            // 
+            this.dgvCompare.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvCompare.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCompare.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.attribute_name,
+            this.MCU1,
+            this.MCU2});
+            this.dgvCompare.Location = new System.Drawing.Point(12, 70);
+            this.dgvCompare.Name = "dgvCompare";
+            this.dgvCompare.Size = new System.Drawing.Size(889, 567);
+            this.dgvCompare.TabIndex = 3;
+            // 
+            // attribute_name
+            // 
+            this.attribute_name.HeaderText = "Attribute Name";
+            this.attribute_name.Name = "attribute_name";
+            this.attribute_name.ReadOnly = true;
+            this.attribute_name.Width = 300;
+            // 
+            // MCU1
+            // 
+            this.MCU1.HeaderText = "MCU 1";
+            this.MCU1.Name = "MCU1";
+            this.MCU1.Width = 200;
+            // 
+            // MCU2
+            // 
+            this.MCU2.HeaderText = "MCU 2";
+            this.MCU2.Name = "MCU2";
+            this.MCU2.Width = 200;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cb_MCU1);
+            this.groupBox1.Controls.Add(this.cb_MCU2);
+            this.groupBox1.Controls.Add(this.btnCompare);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(690, 51);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select MCU to compare";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 649);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvCompare);
+            this.Name = "Form1";
+            this.Text = "STMCUCompare";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCompare)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb_MCU1;
+        private System.Windows.Forms.ComboBox cb_MCU2;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.DataGridView dgvCompare;
+        private System.Windows.Forms.DataGridViewTextBoxColumn attribute_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MCU1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MCU2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ STMCUCompare/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.Data.Sqlite;
+// https://github.com/dotnet/efcore
+
+namespace STMCUCompare
+{
+    public partial class Form1 : Form
+    {
+        string baseName = "cube-finder-db.db";
+
+        List<ComboboxItem> cpns = new List<ComboboxItem>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            if(!System.IO.File.Exists(baseName))
+            {
+                MessageBox.Show("Extract cube-finder-db.db from STM32CubeIDE\\plugins\\com.st.stm32cube.common.mx_*\\db\\plugins\\mcufinder\\mcu\\cube-finder-db.zip or C:\\Program Files\\STMicroelectronics\\STM32Cube\\STM32CubeMX\\db\\plugins\\mcufinder\\mcu into current dirrectory", "cube-finder-db.db not found",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+                return;
+            }
+
+            using (var connection = new SqliteConnection("Data Source=" + baseName))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT id, cpn FROM cpn WHERE cpn like 'STM32F%' ORDER BY cpn";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        cpns.Add(new ComboboxItem(reader.GetString(1), reader.GetInt16(0)));
+                        //var url = reader.GetString(0);
+                    }
+                }
+            }
+
+
+
+
+
+
+            for (int i = 0; i < cpns.Count; i++)
+            {
+                cb_MCU1.Items.Add(cpns[i]);
+                cb_MCU2.Items.Add(cpns[i]);
+
+            }
+
+            cb_MCU1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
+            cb_MCU1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            cb_MCU1.AutoCompleteSource = AutoCompleteSource.ListItems;
+
+            cb_MCU2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown;
+            cb_MCU2.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            cb_MCU2.AutoCompleteSource = AutoCompleteSource.ListItems;
+
+
+
+
+            /*
+                        var connection = new SQLiteConnection("Data Source=" + baseName);
+                        SQLitePCL.raw.SetProvider(new SQLite3Provider_e_sqlite3());
+                        connection.Open();
+
+
+                        using (SqliteCommand fmd = connection.CreateCommand())
+                        {
+                            fmd.CommandText = @"SELECT DISTINCT * FROM cpn";
+                            fmd.CommandType = CommandType.Text;
+                            SqliteDataReader r = fmd.ExecuteReader();
+                            while (r.Read())
+                            {
+                                r = r;
+                                //ImportedFiles.Add(Convert.ToString(r["FileName"]));
+                            }
+
+                        }*/
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            ComboboxItem mcu1 = (ComboboxItem)cb_MCU1.SelectedItem;
+            ComboboxItem mcu2 = (ComboboxItem)cb_MCU2.SelectedItem;
+
+            if (mcu1 == null || mcu2 == null)
+            {
+                return;
+            }
+
+            dgvCompare.Rows.Clear();
+            dgvCompare.Columns[1].HeaderText = (String)mcu1.Text;
+            dgvCompare.Columns[2].HeaderText = (String)mcu2.Text;
+
+            using (var connection = new SqliteConnection("Data Source=" + baseName))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT att.sourceName, att.type, att.unit, cpnha1.strValue as mcu1val, cpnha2.strValue as mcu2val  FROM attribute att  " +
+                                        "left join cpn_has_attribute cpnha1 on att.id = cpnha1.attribute_id AND cpnha1.cpn_id = " + mcu1.Value + " " +
+                                        "left join cpn_has_attribute cpnha2 on att.id = cpnha2.attribute_id AND cpnha2.cpn_id = " + mcu2.Value + " " +
+                                        "where cpnha1.cpn_id is not null or cpnha2.cpn_id is not null " +
+                                        "group by att.id " +
+                                        "order by att.sourceName";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        DataGridViewRow row = (DataGridViewRow)dgvCompare.Rows[0].Clone();
+                        row.Cells[0].Value = reader.GetString(0);
+                        if (!reader.IsDBNull(3))
+                        {
+                            row.Cells[1].Value = reader.GetString(3);
+                        }
+
+                        if (!reader.IsDBNull(4))
+                        {
+                            row.Cells[2].Value = reader.GetString(4);
+                        }
+
+                        String val1 = (String)row.Cells[1].Value;
+                        String val2 = (String)row.Cells[2].Value;
+
+
+                        if (String.Compare((String)row.Cells[1].Value, (String)row.Cells[2].Value) != 0)
+                        {
+                            row.DefaultCellStyle.BackColor = Color.Yellow;
+                        }
+                        else
+                        {
+                            row.DefaultCellStyle.BackColor = Color.White;
+                        }
+
+                        dgvCompare.Rows.Add(row);
+                    }
+                }
+            }
+
+
+
+
+        }
+    }
+}
